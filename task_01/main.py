f664033cc8faa7fad903c1b2c18a5444ed544bda
Andriy <andygrigs88@gmail.com>
2024-10-05 13:14:23 +0200

    +

--- task_01/main.py
+++ task_01/main.py
@@ -1,13 +1,22 @@
 
+# Реалізація сортування вставками
 def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
+    for element in range(1, len(arr)): # Починаємо з другого елемента
+
+        element_to_isert = arr[element] # Ключовий елемент, який потрібно вставити
+
+        element_before = element - 1 # Індекс для попередніх елементів
+            # Порівнюємо з попередніми елементами  зміщуємо елементи вправо
+        while element_before >= 0 and element_to_isert < arr[element_before]:
+
+            arr[element_before + 1] = arr[element_before] # Зсуваємо елемент вправо
+
+            element_before -= 1 # Переходимо до попереднього елемента
+
+        arr[element_before + 1] = element_to_isert  # Вставляємо ключовий елемент у правильне місце
     return arr
 
 
 print(insertion_sort([12, 11, 13, 5, 6]))
+
+# Реалізація сортування злиттям
